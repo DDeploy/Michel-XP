1a3f914148913a74a8bbdf3a9defcedb1be6c1ae
Deployerfull Geek <deployergamerfr@gmail.com>
2019-12-25 18:23:35 +0100

    [JS] Les objets
    • Ajout des objets "soirée"
    • Test d'affichage en faisant "*"
    • innerHTML = variable résultat

--- B-Query.js
+++ B-Query.js
@@ -1,3 +1,24 @@
-var q = document.getElementById('Searching').value;
+function Go_BQuery() {
+    var q = document.getElementById('Searching').value;
+    if (q == "*") {
+        document.getElementById("rep").innerHTML = resultat.soiree[0].name;
+    }
+}
+
+var resultat = {
+    "soiree":[ 
+        {"name":"LMAO Night", "info":[
+            {"adress":"25, Rue du ku à Lyon"},
+            {"price":50}
+        ]}
+        {"name":"XParty", "info":[
+            {"adress":"Ionis Campus à Lyon"},
+            {"price":20}
+        ]}
+    ]
+} 
+//     "Crepes Party", "Chez Vincent"]}
+
+/*var q = document.getElementById('Searching').value;
 var v = "PTDR";
-document.getElementById("rep").innerHTML = q;
+document.getElementById("rep").innerHTML = q;*/
