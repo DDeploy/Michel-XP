140826a60128c793b4e78dd2f7fc0eb8aee7ea72
Deployerfull Geek <deployergamerfr@gmail.com>
2019-12-29 00:15:01 +0100

    [Beautify + Mise en place] Moteur JS Bquery en place
    • Création d'une forêt de "if" pour le moteur
    • Gestion des cas de figures
    • Error 404
    • Erreur lorsque aucun caractère n'est sélectionné
    • Parsing du js

--- B-Query.js
+++ B-Query.js
@@ -1,22 +1,45 @@
 function Go_BQuery() {
     var q = document.getElementById('Searching').value;
-    if (q == "*") {
-        document.getElementById("rep").innerHTML = resultat.soiree[0].name;
+    if (!(q >= 48 && q <= 57) &&
+        !(q >= 65 && q <= 90) &&
+        !(q >= 97 && 122)) {
+        if (q == "")
+            document.getElementById("rep").innerHTML = "Error, you have to write an input please.";
+        else if (resultat.soiree[0].name.includes(q)) {
+            document.getElementById("rep").innerHTML = resultat.soiree[0].name;
+        } else if (resultat.soiree[1].name.includes(q)) {
+            document.getElementById("rep").innerHTML = resultat.soiree[1].name;
+        } else if (resultat.soiree[0].info[0].adress.includes(q)) {
+            document.getElementById("rep").innerHTML = resultat.soiree[0].name[0].adress;
+        } else {
+            document.getElementById("rep").innerHTML = 'Error 404, \'' + q + '\' n\'a pas été trouvé';
+        }
     }
 }
 
 var resultat = {
-    "soiree":[ 
-        {"name":"LMAO Night", "info":[
-            {"adress":"25, Rue du ku à Lyon"},
-            {"price":50}
-        ]}
-        {"name":"XParty", "info":[
-            {"adress":"Ionis Campus à Lyon"},
-            {"price":20}
-        ]}
+    "soiree": [{
+            "name": "LMAO Night",
+            "info": [{
+                    "adress": "25, Rue du ku à Lyon"
+                },
+                {
+                    "price": 50
+                }
+            ]
+        },
+        {
+            "name": "XParty",
+            "info": [{
+                    "adress": "Ionis Campus à Lyon"
+                },
+                {
+                    "price": 20
+                }
+            ]
+        }
     ]
-} 
+}
 //     "Crepes Party", "Chez Vincent"]}
 
 /*var q = document.getElementById('Searching').value;
