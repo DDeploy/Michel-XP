cbc56515d85e8be678a51eaa4559b7cc86aae186
Deployerfull Geek <deployergamerfr@gmail.com>
2020-01-06 19:30:33 +0100

    [JS] Rework moteur Bquery
    • Ajouts des commentaires
    • Rework depuis la base du moteur

--- B-Query.js
+++ B-Query.js
@@ -1,47 +1,51 @@
-function Go_BQuery() {
-    var q = document.getElementById('Searching').value;
-    if (!(q >= 48 && q <= 57) &&
-        !(q >= 65 && q <= 90) &&
-        !(q >= 97 && 122)) {
-        if (q == "")
-            document.getElementById("rep").innerHTML = "Error, you have to write an input please.";
-        else if (resultat.soiree[0].name.includes(q)) {
-            document.getElementById("rep").innerHTML = resultat.soiree[0].name;
-        } else if (resultat.soiree[1].name.includes(q)) {
-            document.getElementById("rep").innerHTML = resultat.soiree[1].name;
-        } else if (resultat.soiree[0].info[0].adress.includes(q)) {
-            document.getElementById("rep").innerHTML = resultat.soiree[0].name[0].adress;
-        } else {
-            document.getElementById("rep").innerHTML = 'Error 404, \'' + q + '\' n\'a pas été trouvé';
+//Résultats disponibles
+var resultats = [
+    {
+        "name": "LMAO Night",
+        "info": {
+            "adress": "25, Rue du ku à Lyon",
+            "price": 50
+        }
+    },
+    {
+        "name": "XParty",
+        "info": {
+            "adress": "Ionis Campus à Lyon",
+            "price": 20
         }
     }
-}
+];
+//Fonction appelée lorsque l'on clique sur le bouton
+function Go_bQuery() {
+    //Contenu du champ de recherche
+    var texte = document.getElementById('Searching').value;
+    //Node HTML de résultats
+    var resultat = document.getElementById('rep');
 
-var resultat = {
-    "soiree": [{
-            "name": "LMAO Night",
-            "info": [{
-                    "adress": "25, Rue du ku à Lyon"
-                },
-                {
-                    "price": 50
-                }
-            ]
-        },
-        {
-            "name": "XParty",
-            "info": [{
-                    "adress": "Ionis Campus à Lyon"
-                },
-                {
-                    "price": 20
-                }
-            ]
+    //Si la valeur du champ de recherche n'est pas bon
+    if (!isAlphaNumeric(texte)) {
+        resultat.innerHTML = "Veuillez entrer un texte alphanumérique";
+        return; //On s'arrete la
+    }
+
+    //Pour chaque donnée de la liste, on la parcours pour trouver celui recherchée
+    for (var soireeIndex = 0; soireeIndex < resultats.length; soireeIndex++) {
+        var soiree = resultats[soireeIndex];
+        //Si le résultat correspond a la recherche
+        if (
+            soiree.name.includes(texte) ||
+            soiree.info.adress.includes(texte)
+        ) {
+            //On affiche le résultat
+            resultat.innerHTML = "Name: " + soiree.name + " | Adresse: " + soiree.info.adress + " | Prix: " + soiree.info.price;
+            return; //On s'arrete la
         }
-    ]
+    }
+    //Si aucun résultat n'est trouvé
+    resultat.innerHTML = "Aucune soiree trouvee.";
 }
-//     "Crepes Party", "Chez Vincent"]}
 
-/*var q = document.getElementById('Searching').value;
-var v = "PTDR";
-document.getElementById("rep").innerHTML = q;*/
+//Détermine si la valeur est alphanumérique
+function isAlphaNumeric(value) {
+    return /^[a-zA-Z0-9]+$/.test(value)
+}
